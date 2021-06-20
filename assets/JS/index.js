7f90a7918ffd1f496720d56c89d861f53b7ab4b6
karuu-01 <brian.muchori@student.moringaschool.com>
2021-06-21 00:46:27 +0300

    add all changes and intergrations

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -93,4 +93,16 @@ $(document).ready(function(){
     $("#product-image").show('1500');
   });
 });
-
+$(document).ready(function() {
+  $("#mc-embedded-subscribe").click(function() {
+    var name = $("input#mce-FNAME").val();
+    var email = $("input#mce-EMAIL").val();
+    var message = $("input#mce-MMERGE6").val();
+    if ($("input#mce-FNAME").val() && $("input#mce-EMAIL").val()){
+      alert (name + ", your message has been received successfully. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!")
+    }
+  });
+});
